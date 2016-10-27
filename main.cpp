5717736d7298273c2b8c6431adb7cf69943ff6b3
John Congote <jcongote@gmail.com>
2016-10-27 09:04:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,25 +10,29 @@ int main(int, char**)
     Mat edges;
     namedWindow( "edges",0);
     resizeWindow("edges", 500,500);
-    /*VideoCapture cap1(2);
+    
+    VideoCapture cap1(2);
       if(!cap1.isOpened())  
         return -1;
 
     Mat edges1;
-    namedWindow("edges1",2);*/
+    namedWindow("edges1",2);
 
     for(;;)
     {
         Mat frame;
         cap >> frame; 
 
-        /*Mat frame1;
-        cap1 >> frame1;*/
+        Mat frame1;
+        cap1 >> frame1;
         imshow("edges", frame);
-        //imshow("edges1", frame1);
+        imshow("edges1", frame1);
         if(waitKey(30) != -1){
             cvDestroyWindow("edges");
-            //cvDestroyWindow("edges1");
+            cvDestroyWindow("edges1");
+            
+            cap.release();
+            cap1.release();            
             break;
         } 
     }
