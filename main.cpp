60f860fa3cf2d5d971635bf7460ebfa0d18773b7
John Congote <jcongote@gmail.com>
2016-10-27 09:02:36 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+int main(int, char**)
+{
+    VideoCapture cap(0); 
+    if(!cap.isOpened())  
+        return -1;
+    Mat edges;
+    namedWindow( "edges",0);
+    resizeWindow("edges", 500,500);
+    /*VideoCapture cap1(2);
+      if(!cap1.isOpened())  
+        return -1;
+
+    Mat edges1;
+    namedWindow("edges1",2);*/
+
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; 
+
+        /*Mat frame1;
+        cap1 >> frame1;*/
+        imshow("edges", frame);
+        //imshow("edges1", frame1);
+        if(waitKey(30) != -1){
+            cvDestroyWindow("edges");
+            //cvDestroyWindow("edges1");
+            break;
+        } 
+    }
+    return 0;
+}
+
